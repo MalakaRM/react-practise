3cfe3f7962d50d9ad4cb18b450971db9aa4a37c6
MalakaRM <160582947+MalakaRM@users.noreply.github.com>
2026-09-17 09:59:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import HeaderContent from "./Components/HeaderContent/HeaderContent.tsx";
+import BodyContent from "./Components/BodyContent/BodyContent.tsx";
+import FooterContent from "./Components/FooterConent/FooterContent.tsx";
+function App() {
+
+
+  return (
+      <>
+       <div id="wrapper">
+       <HeaderContent/>
+       <BodyContent/>
+       <FooterContent/>
+
+
+       </div>
+      </>
+
+
+  )
+}
+
+export default App
---
+++ src/Components/BodyContent/BodyContent.tsx
@@ -0,0 +1,13 @@
+import './BodyConent.css'
+
+function BodyContent() {
+    return (
+        <div id="bodycontent">
+            <h3>body Content</h3>
+        </div>
+
+    )
+}
+
+export default BodyContent
+
